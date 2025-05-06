ec8b8536c20b5ccd93abd7b128d91464445deba6
DarioKawa <1382017@cegepmontpetit.ca>
2025-05-06 15:08:48 -0400

    test

--- src/main/java/org/kickmyb/server/account/ServiceAccount.java
+++ src/main/java/org/kickmyb/server/account/ServiceAccount.java
@@ -6,9 +6,21 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 // extends UserDetailsService which is one of the Spring Security entry points
 public interface ServiceAccount extends UserDetailsService {
 
-    class UsernameTooShort extends Exception {}
-    class UsernameAlreadyTaken extends Exception {}
-    class PasswordTooShort extends Exception {}
+    class UsernameTooShort extends Exception {
+        public UsernameTooShort() {
+            super("Username too short");
+        }
+    }
+    class UsernameAlreadyTaken extends Exception {
+        public UsernameAlreadyTaken() {
+            super("Username already taken");
+        }
+    }
+    class PasswordTooShort extends Exception {
+        public PasswordTooShort() {
+            super("Password too short");
+        }
+    }
 
     void signup(SignupRequest req) throws BadCredentialsException, UsernameTooShort, PasswordTooShort, UsernameAlreadyTaken;
 
